5384c8f711826977b63bffe31d78a36ba593b2b7
Xeraph <jeongdo.dev@gmail.com>
2024-01-07 19:15:07 +0900

    :sparkles: Feat : 다크모드 토글 스위치
    다크모드 토글 스위치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+import ThemeProvider from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <Footer />
+        <ThemeProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
