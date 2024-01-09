51cd5ded935c1d7a09d0d3a9bf447017b857aa86
Xeraph <jeongdo.dev@gmail.com>
2024-01-09 19:41:18 +0900

    :heavy_plus_sign: Chore : MDX 관련 패키지 설치 및 설정
    MDX 관련 패키지 설치 및 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,21 @@ import type { Config } from "tailwindcss";
 
 const { nextui } = require("@nextui-org/react");
 
+/** @type {import('tailwindcss').Config} */
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./mdx-components.tsx",
   ],
   theme: {
     extend: {
+      colors: {
+        "custom-color": "#272b33f",
+      },
       minHeight: {
         screen: "100vh",
         // 추가하려는 다른 min-height 값
@@ -20,8 +26,16 @@ const config: Config = {
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    hljs: {
+      theme: "atom-one-dark",
+    },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui(), require("tailwind-highlightjs")],
+  safelist: [
+    {
+      pattern: /hljs+/,
+    },
+  ],
 };
 export default config;
