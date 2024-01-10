7171eea798c319ee581ffee8bbc0298fd8f205f2
Xeraph <jeongdo.dev@gmail.com>
2024-01-11 04:36:10 +0900

    :wheelchair: Add : robots, sitemap
    Add : robots, sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import { getAllPosts } from "@/libs/posts";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getAllPosts();
+
+  return [
+    {
+      url: "https://www.jeongdo.xyz",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.jeongdo.xyz/about",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.jeongdo.xyz/pics",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    ...posts.map((post) => {
+      return {
+        url: `https://yceffort.kr/posts/${post.slug}`,
+        lastModified: new Date(post.date),
+        priority: 0.5,
+      };
+    }),
+  ];
+}
