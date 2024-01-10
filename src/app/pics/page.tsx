2a2c43f6dc0dcb1abda2726004acd8184ba2ca29
Xeraph <jeongdo.dev@gmail.com>
2024-01-11 02:28:45 +0900

    Fix : key 설정

--- src/app/pics/page.tsx
+++ src/app/pics/page.tsx
@@ -3,7 +3,6 @@ import IMAGES from "@/assets/inedx";
 
 function Pics() {
   const outer_map = Array(15).fill(0);
-  const inner_map = Array(3).fill(0);
 
   return (
     <div className="flex gap-3 w-full flex-wrap justify-center">
@@ -11,7 +10,7 @@ function Pics() {
         // 타입 단언
         const imageKey = `IMG_${index + 1}` as keyof typeof IMAGES;
         return (
-          <div className="relative sm:w-[190px] sm:h-[190px] w-[100px] h-[100px]">
+          <div className="relative sm:w-[190px] sm:h-[190px] w-[100px] h-[100px]" key={imageKey}>
             <Image
               src={IMAGES[imageKey]}
               alt={`Image_${index + 1}`}
