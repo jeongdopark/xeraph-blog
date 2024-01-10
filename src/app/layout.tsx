7723a61d81a4d170c6863e816dcfee4c9a92bc85
Xeraph <jeongdo.dev@gmail.com>
2024-01-11 04:56:58 +0900

    :heavy_plus_sign: Add : google verification
    Add : google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content={process.env.GOOGLE_CONTENT} />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="dark:bg-dark-color dark:text-white">
