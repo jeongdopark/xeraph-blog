a632c71bd1f6ed763d26791660cffe2a5ad1b28a
Xeraph <jeongdo.dev@gmail.com>
2024-01-10 16:37:04 +0900

    :lipstick: Style : Pic Page
    Pic Page

--- src/app/pics/page.tsx
+++ src/app/pics/page.tsx
@@ -1,5 +1,38 @@
+import Image from "next/image";
+import IMAGES from "@/assets/inedx";
+
 function Pics() {
-  return <div>Pics</div>;
+  const outer_map = Array(5).fill(0);
+  const inner_map = Array(3).fill(0);
+
+  let count = 1;
+
+  return (
+    <div className="flex-col gap-3 flex w-full">
+      {outer_map.map((_, outerIndex) => (
+        <div key={`row_${outerIndex}`} className="flex gap-3">
+          {inner_map.map((_, innerIndex) => {
+            // 타입 단언
+            const imageKey = `IMG_${count++}` as keyof typeof IMAGES;
+
+            return (
+              <div key={`img_${outerIndex}_${innerIndex}`}>
+                <Image
+                  src={IMAGES[imageKey]}
+                  alt={`Image_${count}`}
+                  width={200}
+                  height={200}
+                  className="rounded-sm"
+                  priority={true}
+                  placeholder="blur"
+                />
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Pics;
---
+++ src/assets/inedx.ts
@@ -0,0 +1,35 @@
+import IMG_1 from "@/assets/pic_1.jpg";
+import IMG_2 from "@/assets/pic_2.jpg";
+import IMG_3 from "@/assets/pic_3.jpg";
+import IMG_4 from "@/assets/pic_4.jpg";
+import IMG_5 from "@/assets/pic_5.jpg";
+import IMG_6 from "@/assets/pic_6.jpg";
+import IMG_7 from "@/assets/pic_7.jpg";
+import IMG_8 from "@/assets/pic_8.jpg";
+import IMG_9 from "@/assets/pic_9.jpg";
+import IMG_10 from "@/assets/pic_10.jpg";
+import IMG_11 from "@/assets/pic_11.jpg";
+import IMG_12 from "@/assets/pic_12.jpg";
+import IMG_13 from "@/assets/pic_13.jpg";
+import IMG_14 from "@/assets/pic_14.jpg";
+import IMG_15 from "@/assets/pic_15.jpg";
+
+const IMAGES = {
+  IMG_1,
+  IMG_2,
+  IMG_3,
+  IMG_4,
+  IMG_5,
+  IMG_6,
+  IMG_7,
+  IMG_8,
+  IMG_9,
+  IMG_10,
+  IMG_11,
+  IMG_12,
+  IMG_13,
+  IMG_14,
+  IMG_15,
+};
+
+export default IMAGES;
