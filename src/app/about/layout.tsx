be729bdf7820c2e5a976187379857886035f94da
Xeraph <jeongdo.dev@gmail.com>
2024-01-12 02:38:00 +0900

    :mag: Add : 각 페이지 meta tag, favicon 사이즈 수정
    Add : 각 페이지 meta tag, favicon 사이즈 수정

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,7 +1,19 @@
 import { ReactNode } from "react";
+import type { Metadata } from "next";
 import Header from "@/components/layout/Header";
+import { openGraphImage } from "../shared-metadata";
 
-export default function AboutLayout({ children, params }: { children: ReactNode; params: any }) {
+export const metadata: Metadata = {
+  title: "Xeraph | about",
+  description: "Xeraph | Personal Blog",
+  openGraph: {
+    ...openGraphImage,
+    title: "jeongdo's blog",
+    description: "jeongdo's blog",
+  },
+};
+
+export default function AboutLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <Header />
--- src/app/pics/layout.tsx
+++ src/app/pics/layout.tsx
@@ -1,5 +1,17 @@
-import Header from "@/components/layout/Header";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+import Header from "@/components/layout/Header";
+import { openGraphImage } from "../shared-metadata";
+
+export const metadata: Metadata = {
+  title: "Xeraph | Pics",
+  description: "Xeraph | Personal Blog",
+  openGraph: {
+    ...openGraphImage,
+    title: "jeongdo's blog",
+    description: "jeongdo's blog",
+  },
+};
 
 export default function PicLayout({ children, params }: { children: ReactNode; params: any }) {
   return (
