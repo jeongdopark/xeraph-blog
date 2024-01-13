4ae7754c14ed8f282e4485d80ab0942ae1206e37
Xeraph <jeongdo.dev@gmail.com>
2024-01-13 15:58:17 +0900

    :lipstick: Style : blockquote 다크모드 색상
    style : blockquote 다크모드 색상

--- src/components/Blockquote.tsx
+++ src/components/Blockquote.tsx
@@ -2,7 +2,7 @@ export default function Blockquote({ text }: any) {
   return (
     <div className="flex text-sm h-auto font-medium mt-7 mb-2">
       <div className="w-[5px] h-auto bg-gray-500"></div>
-      <div className="flex items-center p-3 bg-gray-100 justify-center dark:text-dark-color">
+      <div className="flex items-center p-3 bg-gray-100 justify-center dark:text-dark-color dark:bg-gray-200">
         {text[1].props.children}
       </div>
     </div>
