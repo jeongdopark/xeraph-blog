e0c438d5e6aa76dc45a4b2972431e99745c1795f
Xeraph <jeongdo.dev@gmail.com>
2024-01-16 16:54:49 +0900

    :lipstick: Style : 사진 순회하는 방식 변경
    사진 순회하는 방식 변경

--- src/app/pics/page.tsx
+++ src/app/pics/page.tsx
@@ -1,28 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import IMAGES from "@/assets/inedx";
 
 function Pics() {
-  const outer_map = Array(15).fill(0);
+  const outer = Array(5).fill(0);
+  const inner = Array(3).fill(0);
+  let counter = 0;
 
   return (
-    <div className="flex gap-[7px] w-full flex-wrap sm:gap-[12px]">
-      {outer_map.map((_, index) => {
-        // 타입 단언
-        const imageKey = `IMG_${index + 1}` as keyof typeof IMAGES;
-        return (
-          <div className="relative sm:w-[192px] sm:h-[192px] w-[112px] h-[112px]" key={imageKey}>
-            <Image
-              src={IMAGES[imageKey]}
-              alt={`Image_${index + 1}`}
-              layout="fill"
-              className="rounded-sm"
-              priority={true}
-              placeholder="blur"
-              key={imageKey}
-            />
-          </div>
-        );
-      })}
+    <div className="w-full">
+      <div className="flex-col">
+        {outer.map((col) => {
+          return (
+            <div className="flex justify-center sm:mb-[7px] sm:gap-[7px] mb-[5px] gap-[5px]">
+              {inner.map((row) => {
+                counter += 1;
+                const imageKey = `IMG_${counter}` as keyof typeof IMAGES;
+                return (
+                  <div className="relative sm:w-[192px] sm:h-[192px] w-[112px] h-[112px]" key={imageKey}>
+                    <Image
+                      src={IMAGES[imageKey]}
+                      alt={`Image_${counter}`}
+                      layout="fill"
+                      className="rounded-sm"
+                      priority={true}
+                      placeholder="blur"
+                      key={imageKey}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
