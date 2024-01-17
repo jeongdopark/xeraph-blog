59b22565a6aab0d408ab6e4ffda6ba925320d668
Xeraph <jeongdo.dev@gmail.com>
2024-01-17 17:26:17 +0900

    :recycle: Refacotor : Image optimize
    Image optimize

--- src/components/layout/Author.tsx
+++ src/components/layout/Author.tsx
@@ -1,4 +1,4 @@
-import AuthorImg from "@/assets/author.webp";
+import AuthorImg from "../../assets/author.png";
 import { GITHUB_URL } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,15 +7,7 @@ function Author() {
   return (
     <div className="flex items-center w-full mt-5 mb-10 gap-5 ">
       <div className="w-[60px] h-[60px] rounded-full relative overflow-hidden">
-        <Image
-          src={AuthorImg.src}
-          alt="author img"
-          quality={70}
-          fill
-          placeholder="blur"
-          blurDataURL={AuthorImg.src}
-          sizes="60px"
-        />
+        <Image src={AuthorImg} alt="author img" quality={70} fill placeholder="blur" sizes="60px" />
       </div>
 
       <div className="flex-col">
